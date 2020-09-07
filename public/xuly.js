5afcc5f8f18571efd8cac3ea0469646a17547433
nguyentrongtien <nguyenusb2005@gmail.com>
2020-09-07 15:39:21 +0700

    add file xuly.js to render more!

---
+++ public/xuly.js
@@ -0,0 +1,13 @@
+ReactDOM.render(
+    <div>
+        <h1 className="yellow">TK SOFT!</h1>
+        <h1>TK SOFT!</h1>
+    </div>
+, document.getElementById('root'));
+
+ReactDOM.render(
+    <div>
+        <h1 className="yellow">TK SOFT!</h1>
+        <h1>TK SOFT!</h1>
+    </div>
+, document.getElementById('root_2'));
