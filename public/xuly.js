13a42607a6c4440fbc7f44d94c58ee185c628458
nguyentrongtien <nguyenusb2005@gmail.com>
2020-09-08 16:02:10 +0700

    add props and onClick event!

--- public/xuly.js
+++ public/xuly.js
@@ -1,13 +1,34 @@
-ReactDOM.render(
-    <div>
-        <h1 className="yellow">TK SOFT!</h1>
-        <h1>TK SOFT!</h1>
-    </div>
-, document.getElementById('root'));
+
+var Tksoft = React.createClass({
+    getInfo: function(){
+        alert(this.props.children);
+    },
+    render: function(){
+        return(
+            <div>
+                <h1 className="yellow">{this.props.ten}-{this.props.giangvien}</h1>
+                <p>{this.props.children}</p>
+                <button onClick={this.getInfo}>Get Info!</button>
+                <Sub></Sub> 
+            </div>    
+        );
+    }
+});
+var Sub = React.createClass({
+    render: function(){
+        return(
+            <h3 className="yellow">Subject!</h3>
+        );
+    }
+});
 
 ReactDOM.render(
     <div>
-        <h1 className="yellow">TK SOFT!</h1>
-        <h1>TK SOFT!</h1>
+        <Tksoft ten="Node JS" giangvien="Mr Khoa!">Node JS Tien</Tksoft>
+        <Tksoft ten="React JS" giangvien="Mr Pho!">React JS Tien</Tksoft>
     </div>
-, document.getElementById('root_2'));
+, document.getElementById('root'));
+
+
+
+
