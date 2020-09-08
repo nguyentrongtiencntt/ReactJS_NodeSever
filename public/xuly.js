66f54acb37733d71331a2845be6581b34f7d3657
nguyentrongtien <nguyenusb2005@gmail.com>
2020-09-08 18:02:38 +0700

    state trong component

--- public/xuly.js
+++ public/xuly.js
@@ -1,18 +1,32 @@
-function getName(){
-    alert("Call function from out of component!");
+function getName(name){
+    alert(name);
 }
 
 
 var Tksoft = React.createClass({
+    addstudent: function() {
+        this.setState({tongHocVien: this.state.tongHocVien + 1});
+    }
+    ,
     getInfo: function(){
         alert(this.props.children);
     },
+    getInitialState(){
+        return {
+            tongHocVien: 10
+        };
+    }
+    ,
     render: function(){
         return(
             <div>
                 <h1 className="yellow">{this.props.ten}-{this.props.giangvien}</h1>
+                <div>So hoc vien: {this.state.tongHocVien}</div>
                 <p>{this.props.children}</p>
-                <button onClick={getName}>Get Info!</button>
+                <button onClick={()=>{ var str=this.props.giangvien;
+                    getName(str)
+                    }}>Get Info!</button>
+                <button onClick={this.addstudent} >Add student!</button>
                 <Sub></Sub> 
             </div>    
         );
